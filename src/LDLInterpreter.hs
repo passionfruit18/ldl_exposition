a633367b1ef2f9be998463d462c1562c28564a2b
James Dai <james.dai.uk@googlemail.com>
2016-02-08 01:33:18 +0000

    changes to printing

--- src/LDLInterpreter.hs
+++ src/LDLInterpreter.hs
@@ -18,16 +18,22 @@ ldl_pretty_print = logic_pretty_print
 	(\u s -> "<" ++ reg_pretty_print u ++ ">" ++ s)
 	(\b s1 s2 -> "")
 
+sep1 = take 60 (repeat '=')
+sep2 = take 60 (repeat '-')
 ldl_respond :: LDLogic_ -> String
 ldl_respond ldl =
-	let s1 = ldl_pretty_print ldl;
+	let s1 = ldl_nnf_pretty_print $ ldl2nnf ldl;
 		alphabet = subsets (S.fromList $ ldl_propositions ldl);
 		afa = ldl2afa alphabet ldl;
 		s2 = ldl2afa_pretty_print afa;
-		s3 = show $ a2nReach afa in
-	unlines ["LDL: " ++ s1,
-				"AFA: " ++ s2,
-				"NFA: " ++ s3] 
+		s3 = ldl2afa2nfa_pretty_print $ a2nReach afa in
+	unlines [sep1,
+			"LDL: " ++ s1,
+			sep2,
+			"AFA: " ++ s2,
+			sep2,
+			"NFA: " ++ s3,
+			sep1] 
 
 
 reg_pretty_print :: Reg_ -> String
@@ -47,6 +53,7 @@ reg_nnf_pretty_print =
 	(connect ";")
 	(++"*")
 	. op RegNNF
+
 ldl_nnf_pretty_print :: Show p => LDLogicNNF p -> String
 ldl_nnf_pretty_print = 
 	foldLog show show -- propositional constants and variables
@@ -58,24 +65,63 @@ ldl_nnf_pretty_print =
 		f (Diamond x) s = "<" ++ reg_nnf_pretty_print x ++ ">" ++ s
 		f (Square x) s = "[" ++ reg_nnf_pretty_print x ++ "]" ++ s
 
+fa_headings = ["Alphabet: ", "States: ", "Init: ", "Transition: ", "Final: "]
 afa_pretty_print :: (Show q, Show a, Ord q, Ord a) => AFA q a -> String
 afa_pretty_print (FA as qs q t fs) =
 	unlines $
 	zipWith (++)
-	["Alphabet: ", "States: ", "Init: ", "Transition: ", "Final: "]
+	fa_headings
 	[show $ ShowSet as, show $ ShowSet qs, show q, show t, show $ ShowSet fs]
 
 ldl2afa_pretty_print :: (Show p, Ord p) => AFA (LDLogicNNF p) (S.Set p) -> String
 ldl2afa_pretty_print (FA as qs q t fs) =
+	let pr = ldl_nnf_pretty_print in
 	unlines $
 	zipWith (++)
-	["Alphabet: ", "States: ", "Init: ", "Transition: ", "Final: "]
+	fa_headings
 	[show $ ShowSet $ S.map ShowSet as,
-	show $ ShowSet $ S.map ldl_nnf_pretty_print qs,
-	ldl_nnf_pretty_print q,
-	unlines $ map show $ map (\((q1,a),q2) -> (ldl_nnf_pretty_print q1) ++ " X " ++ (show $ ShowSet a) ++ " -> " ++ (show q2)) $ M.assocs $ op ATransition t,
-	show $ ShowSet $ S.map ldl_nnf_pretty_print fs]
+	showSetWith pr qs,
+	pr q,
+	unlines
+			$ map (\((q1,a),q2) -> (pr q1) ++
+									" X " ++
+									(show $ ShowSet a) ++
+									" -> " ++
+									(show q2))
+			$ M.assocs $ op ATransition t,
+	showSetWith pr fs]
 
+show_setOfset_with :: (Ord p) => (p -> String) -> S.Set (S.Set p) -> String
+show_setOfset_with f = show . ShowSet . S.map (ShowSet . S.map f)
+
+show_set_with :: (Ord p) => (p -> String) -> S.Set p -> String
+show_set_with f = show . ShowSet . S.map f
+
+ldl2afa2nfa_pretty_print :: (Show p, Ord p) => NFA (S.Set (LDLogicNNF p)) (S.Set p) -> String
+ldl2afa2nfa_pretty_print (FA as qs q t fs) =
+	let pr = ldl_nnf_pretty_print in
+	unlines $
+	zipWith (++)
+	fa_headings
+	[show $ ShowSet $ S.map ShowSet as,
+	showSetOfSetWith pr qs,
+	showSetWith pr q,
+	unlines 
+			$ map (\((q1, a), q2) -> showSetWith pr q1 ++
+									" X " ++
+									(show $ ShowSet a) ++
+									" -> " ++
+									showSetOfSetWith pr q2)
+			$ M.assocs
+			$ op NTransition t,
+	showSetOfSetWith pr fs]
+
+showSetWith :: Ord q => (q -> String) -> S.Set q -> String
+showSetWith g qs = if S.null qs then "{}" else "{" ++ foldr1 f (map g $ S.toList qs) ++ "}"
+					where f x xs = (x ++ ", " ++ xs)
+
+showSetOfSetWith :: Ord q => (q -> String) -> (S.Set (S.Set q)) -> String
+showSetOfSetWith g = showSetWith (showSetWith g)
 
 newtype ShowSet q = ShowSet (S.Set q) deriving (Eq, Ord)
 instance (Show q, Ord q) => Show (ShowSet q) where
